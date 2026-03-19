7c946d1a216f35126a7890ed8520876c6b591877
Zachary Dovel <zachary@Slugger.local>
2026-03-19 22:03:11 +0900

    add pages

---
+++ app/videos/page.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next';
+
+type Repo = {
+  data: {
+    name: string
+    stargazers_count: number
+  }
+}
+
+const Page: NextPage<{data : Repo }> = () => {
+  return null;
+};
+
+export default Page;
